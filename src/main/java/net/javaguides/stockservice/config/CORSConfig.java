03aed1da5bf52a3f7f4ce7dd32ed68492541d3e8
nadiah-m <nadiah.malik@Hotmail.com>
2025-01-02 12:35:44 +0800

    update cors

--- src/main/java/net/javaguides/stockservice/config/CORSConfig.java
+++ src/main/java/net/javaguides/stockservice/config/CORSConfig.java
@@ -10,7 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow CORS for all paths
-                .allowedOrigins("http://localhost:3000")  // Allow the React frontend's URL
+                .allowedOrigins("http://localhost")  // Allow the React frontend's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow these methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true)  // Allow cookies to be sent with requests
